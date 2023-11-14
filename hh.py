9d75c482127f0a248a180df638c12d6746a5a618
luxingjie <1239247323@qq.com>
2023-11-14 19:49:23 +0000

    made zhen de hen fan

---
+++ hh.py
@@ -0,0 +1,14 @@
+# XingJieXingjie
+# XingJieXingjie
+#import sys
+#print(sys.version)
+#print(sys.version_info)
+
+# 可以直接cd加路径找到python，dir确定， 然后python filename.py跑文件
+fruit = 'banana'
+letter = fruit[1]
+print(letter)
+x=3
+w = fruit[x-1]
+print(w)
+print(len(fruit)) # len funvtion is some stored code 计算长度
